fa0fd0d8c2fec6936c665f229ffd9dc251c7ec6b
nandanam <nandanamn675@gmail.com>
2023-12-19 23:21:58 +0530

    third commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Todo from "./Components/Todo"
+import TodoItems from "./Components/Todoitems"
+
+const App = () =>{
+  return(
+    <div>
+      <Todo/>
+    </div>
+ 
+  )
+}
+export default App
